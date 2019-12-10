2db2298548d8b75f84ccc379bfde59b9f41bcb26
trybotycki <t.rybotycki@dpcpolska.pl>
2019-12-10 10:03:52 +0100

    Work in progress on day 10.

---
+++ 10/10.py
@@ -0,0 +1,23 @@
+def get_solution_to_problem_1():
+    space_map = get_space_map_from_input('test1')
+
+
+def get_space_map_from_input(input_path):
+    file = open(input_path)
+    lines = file.readlines()
+    file.close()
+
+    space_map = []
+
+    for line in lines:
+        space_map.append([])
+
+    for i in range(len(lines[0]):
+        for j in range
+
+    print(lines[0][1])
+    return lines
+
+
+
+get_solution_to_problem_1()
