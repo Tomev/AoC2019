11de2a303aab279cb78f1248c9868763f653b426
trybotycki <t.rybotycki@dpcpolska.pl>
2019-12-10 09:33:42 +0100

    Solution to day 9.

--- 9/9.py
+++ 9/9.py
@@ -11,4 +11,14 @@ def get_solution_to_problem_1():
     cpu.run_program()
 
 
+def get_solution_to_problem_2():
+    cpu = ShipComputer()
+    cpu.input.append(2)
+    program = get_program_from_input('input')
+    # program = get_program_from_input('test3')
+    cpu.set_program(program)
+    cpu.run_program()
+
+
 get_solution_to_problem_1()
+get_solution_to_problem_2()
